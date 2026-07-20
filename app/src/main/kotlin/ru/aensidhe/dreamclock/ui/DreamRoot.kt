d311b0f57ad5ceeb079be5b789534b4daf3aa6df
Anatoly Popov <me@aensidhe.ru>
2026-07-20 08:28:38 +0300

    feat: :robot: thread the photo deck and suppression through DreamRoot

--- app/src/main/kotlin/ru/aensidhe/dreamclock/ui/DreamRoot.kt
+++ app/src/main/kotlin/ru/aensidhe/dreamclock/ui/DreamRoot.kt
@@ -3,7 +3,12 @@ package ru.aensidhe.dreamclock.ui
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import coil3.ImageLoader
 import ru.aensidhe.dreamclock.ui.colorrender.ColorRenderMode
 
 @Composable
@@ -11,13 +16,19 @@ fun DreamRoot(
     state: ClockUiState,
     showAnalog: Boolean,
     mode: ColorRenderMode,
+    deck: SlideDeckModel?,
+    imageLoader: ImageLoader?,
 ) {
+    var suppressBottomLeft by remember { mutableStateOf(false) }
     Box(Modifier.fillMaxSize()) {
         SlideDeck(
+            deck = deck,
+            imageLoader = imageLoader,
             showAnalog = showAnalog,
             now = java.time.LocalDateTime.now(),
             secondHandColor = stateColor(state.state),
+            onSuppressBottomLeft = { suppressBottomLeft = it },
         )
-        ClockOverlay(ui = state, mode = mode)
+        ClockOverlay(ui = state, mode = mode, suppressBottomLeft = suppressBottomLeft)
     }
 }
